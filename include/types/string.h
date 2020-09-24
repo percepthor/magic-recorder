e5f9dd9a7bfc4836f99cdf8a4cc92de3d0febf37
ermiry <erick_salas@ermiry.com>
2020-09-24 08:45:01 -0500

    Added types sources

---
+++ include/types/string.h
@@ -0,0 +1,57 @@
+#ifndef _TYPES_STRING_H_
+#define _TYPES_STRING_H_
+
+#include "cerver/types/types.h"
+
+#include "cerver/config.h"
+
+typedef struct String {
+
+	unsigned int len;
+	char *str;
+
+} String;
+
+extern String *str_new (const char *str);
+
+extern void str_delete (void *str_ptr);
+
+extern String *str_create (const char *format, ...);
+
+extern int str_compare (const String *s1, const String *s2);
+
+extern int str_comparator (const void *a, const void *b);
+
+extern void str_copy (String *to, String *from);
+
+extern void str_replace (String *old, const char *str);
+
+// concatenates two strings into a new one
+extern String *str_concat (String *s1, String *s2);
+
+// appends a char to the end of the string
+// reallocates the same string
+extern void str_append_char (String *s, const char c);
+
+// appends a c string at the end of the string
+// reallocates the same string
+extern void str_append_c_string (String *s, const char *c_str);
+
+extern void str_to_upper (String *string);
+
+extern void str_to_lower (String *string);
+
+extern char **str_split (String *string, const char delim, int *n_tokens);
+
+extern void str_remove_char (String *string, char garbage);
+
+// removes the last char from a string
+extern void str_remove_last_char (String *string);
+
+// check if a str (to_find) is inside str
+// returns 0 on exact match
+// returns 1 if it match the letters but len is different
+// returns -1 if no match
+extern int str_contains (String *str, char *to_find);
+
+#endif
---
+++ include/types/types.h
@@ -0,0 +1,28 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef unsigned char ascii_char;
+typedef unsigned char uchar;
+
+// takes no argument and returns a value (int)
+typedef u8 (*Func)(void);
+
+// takes an argument and does not return a value
+typedef void (*Action)(void *);
+
+// takes an argument and returns a value (int)
+typedef u8 (*delegate)(void *);
+
+#endif
